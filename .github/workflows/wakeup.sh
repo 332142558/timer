ddc289c66a07ddba5183c6d9e86a1bfbbbd6564e
332142558 <58900903+332142558@users.noreply.github.com>
2024-09-10 18:01:47 +0800

    Update wakeup.sh

--- .github/workflows/wakeup.sh
+++ .github/workflows/wakeup.sh
@@ -12,7 +12,7 @@ curl 'https://glados.space/api/user/checkin' \
   -H 'accept-language: zh-CN,zh;q=0.9' \
   -H 'authorization: 65514417340718653465740470278038-831-1477' \
   -H 'content-type: application/json;charset=UTF-8' \
-  -H 'cookie: koa:sess=eyJjb2RlIjoiSjlDMTktTVE0WVctUEUwRzktNjJGUDMiLCJ1c2VySWQiOjQyNzIxOCwiX2V4cGlyZSI6MTcyNDkyMTY4NDQwMywiX21heEFnZSI6MjU5MjAwMDAwMDB9; koa:sess.sig=dnc8EzRou2DHfOLd6ubaC-sgCEo; _gid=GA1.2.2055581208.1703044073; _ga_CZFVKMNT9J=GS1.1.1703127080.54.0.1703127080.0.0.0; _ga=GA1.2.873632177.1699001636; _gat_gtag_UA_104464600_2=1' \
+  -H 'cookie: _ga=GA1.1.873632177.1699001636; _ga_CZFVKMNT9J=GS1.1.1717383596.135.1.1717383612.0.0.0; koa:sess=eyJjb2RlIjoiSjlDMTktTVE0WVctUEUwRzktNjJGUDMiLCJ1c2VySWQiOjQyNzIxOCwiX2V4cGlyZSI6MTc1MTg4MjI0OTcxNSwiX21heEFnZSI6MjU5MjAwMDAwMDB9; koa:sess.sig=MaM7OLnr9S2OLZ2bvwXEy_tdFqg' \
   -H 'origin: https://glados.space' \
   -H 'sec-ch-ua: "Chromium";v="118", "Google Chrome";v="118", "Not=A?Brand";v="99"' \
   -H 'sec-ch-ua-mobile: ?0' \
